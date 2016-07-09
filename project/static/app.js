2b9cde06540bef3dc5ffef1f9220d915dccea407
Sairam <sairamch04@gmail.com>
2016-07-10 01:09:43 +0530

    Added files * controllers and services front-end for login, logout and register * HTML views for the controllers

--- project/static/app.js
+++ project/static/app.js
@@ -1,6 +1,35 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('toDoApp', ['ngRoute']);
 
 myApp.config(function ($routeProvider) {
   $routeProvider
-    .when('/', {templateUrl: 'static/partials/home.html'});
+    .when('/', {
+      templateUrl: 'static/partials/home.html',
+      access: {restricted: true}
+    })
+    .when('/login', {
+      templateUrl: 'static/partials/login.html',
+      controller: 'loginController',
+      access: {restricted: false}
+    })
+    .when('/logout', {
+      controller: 'logoutController',
+      access: {restricted: true}
+    })
+    .when('/register', {
+      templateUrl: 'static/partials/register.html',
+      controller: 'registerController',
+      access: {restricted: false}
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
+
+myApp.run(function ($rootScope, $location, $route, AuthService) {
+  $rootScope.$on('$routeChangeStart', function (event, next, current) {
+    if (next.access.restricted && AuthService.isLoggedIn() === false) {
+      $location.path('/login');
+      $route.reload();
+    }
+  });
 });
