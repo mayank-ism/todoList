77003c4ad4a8ae9ca9443edd3e041e0d5f69509c
Sairam <sairamch04@gmail.com>
2016-07-13 16:50:18 +0530

    Added new route for 'todolist' url

--- project/static/app.js
+++ project/static/app.js
@@ -2,37 +2,36 @@ var myApp = angular.module('toDoApp', ['ngRoute']);
 
 myApp.config(function ($routeProvider) {
   $routeProvider
-    .when('/', {
-      templateUrl: 'static/partials/home.html',
-      access: {restricted: true}
-    })
-    .when('/login', {
-      templateUrl: 'static/partials/login.html',
-      controller: 'loginController',
-      access: {restricted: false}
-    })
-    .when('/logout', {
-      controller: 'logoutController',
-      access: {restricted: true}
-    })
-    .when('/register', {
-      templateUrl: 'static/partials/register.html',
-      controller: 'registerController',
-      access: {restricted: false}
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
+  .when('/login', {
+    templateUrl: 'static/partials/login.html',
+    controller: 'loginController',
+    access: {restricted: false}
+  })
+  .when('/logout', {
+    controller: 'logoutController',
+    access: {restricted: true}
+  })
+  .when('/register', {
+    templateUrl: 'static/partials/register.html',
+    controller: 'registerController',
+    access: {restricted: false}
+  })
+  .otherwise({
+   controller : function(){
+    window.location.replace('/todolist');
+    }, 
+    template : "<div></div>"
+   });
 });
 
 myApp.run(function ($rootScope, $location, $route, AuthService) {
   $rootScope.$on('$routeChangeStart', function (event, next, current) {
     AuthService.getUserStatus()
-      .then(function(){
-        if (next.access.restricted && !AuthService.isLoggedIn()){
-          $location.path('/login');
-          $route.reload();
-        }
-      });
+    .then(function(){
+      if (next.access.restricted && !AuthService.isLoggedIn()){
+        $location.path('/login');
+        $route.reload();
+      }
+    });
   });
 });
